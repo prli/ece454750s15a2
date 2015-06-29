fe3810dfbfbfc10e7941aa4ec3fc03f4b408f667
Pei Ran Li <prli@uwaterloo.ca>
2015-06-28 20:19:30 -0400

    init setup for multithreading soln

--- src/ece454750s15a2/TriangleCountImpl.java
+++ src/ece454750s15a2/TriangleCountImpl.java
@@ -18,54 +18,76 @@ public class TriangleCountImpl {
     private int numCores;
 
     public TriangleCountImpl(byte[] input, int numCores) {
-	this.input = input;
-	this.numCores = numCores;
+		this.input = input;
+		this.numCores = numCores;
     }
 
     public List<String> getGroupMembers() {
-	ArrayList<String> ret = new ArrayList<String>();
-	ret.add("prli");
-	ret.add("l22fu");
-	ret.add("p8zhao");
-	return ret;
+		ArrayList<String> ret = new ArrayList<String>();
+		ret.add("prli");
+		ret.add("l22fu");
+		ret.add("p8zhao");
+		return ret;
     }
+	
+	private class TriangleCountRunnable implements Runnable {
 
-    public List<Triangle> enumerateTriangles() throws IOException {
-	// this code is single-threaded and ignores numCores
-
-    	ArrayList<ArrayList<Integer>> adjacencyList = getAdjacencyList(input);
-    	ArrayList<Triangle> ret = new ArrayList<Triangle>();
-    
-    	// naive triangle counting algorithm
-    	int numVertices = adjacencyList.size();
-    	for (int i = 0; i < numVertices; i++) {
-    	    ArrayList<Integer> n1 = adjacencyList.get(i);
-			ConcurrentHashMap<Integer,Integer> hs1 = new ConcurrentHashMap<Integer,Integer> (n1.size());
-			for(int j:n1)
-			{
-				hs1.put(j,1);
-				ArrayList<Integer> temp = adjacencyList.get(j);
-				temp.remove(new Integer(i));
-			}
-     	    for (int j : n1) {
-				
-				if (i > j) {
-					continue;
+		int threadId;
+		List adjacencyList;
+		List ret;
+		public TriangleCountRunnable(int threadId, ArrayList<ArrayList<Integer>> adjacencyList, ArrayList<Triangle> ret) {
+			this.threadId = threadId;
+			this.adjacencyList = Collections.synchronizedList(adjacencyList);
+			this.ret = Collections.synchronizedList(ret);
+		}
+		
+		public void run() {
+			int numVertices = adjacencyList.size();
+			System.out.println("numVertices:" +  numVertices + " ...");
+			for (int i = threadId; i < numVertices; i += numCores) {
+				System.out.println("Thread " +  threadId + " running...");
+				ArrayList<Integer> n1 = (ArrayList<Integer>)adjacencyList.get(i);
+				ConcurrentHashMap<Integer,Integer> hs1 = new ConcurrentHashMap<Integer,Integer> (n1.size());
+				System.out.println("node size:" +  n1.size() + " ...");
+				for(int j:n1)
+				{
+					hs1.putIfAbsent(j,1);
+					// ArrayList<Integer> temp = (ArrayList<Integer>) adjacencyList.get(j);
+					// temp.remove(new Integer(i));
 				}
-				ArrayList<Integer> n2 = adjacencyList.get(j);
-
-				for (int l: n2) {
-					if (j > l) {
+				
+				for (int j : n1) {
+					
+					if (i > j) {
 						continue;
 					}
-					if (hs1.containsKey(l)) {    
-						ret.add(new Triangle(i, j, l));
+					ArrayList<Integer> n2 = (ArrayList<Integer>)adjacencyList.get(j);
+
+					for (int l: n2) {
+						if (j > l) {
+							continue;
+						}
+						if (hs1.containsKey(l)) {    
+							ret.add(new Triangle(i, j, l));
+							System.out.println("Thread " +  threadId + " adding...");
+						}
 					}
 				}
+			}
+		}
+	}
 
-     	    }
-    	}
-    
+    public List<Triangle> enumerateTriangles() throws IOException {
+	
+	    ArrayList<ArrayList<Integer>> adjacencyList = getAdjacencyList(input);
+    	ArrayList<Triangle> ret = new ArrayList<Triangle>();
+	
+		for(int i = 0; i < numCores; i++)
+		{
+			Thread t = new Thread(new TriangleCountRunnable(i, adjacencyList, ret));
+			t.start();
+		}
+
     	return ret;
     }
 
